13e7ecf953c59094819b3850d3a54d16a53079c3
Pedro Leão <pedroleao031@gmail.com>
2025-02-11 13:56:28 -0300

    carrega grafo e get grau
    ambas na classe pai, todos testes deram certo

--- include/Grafo.h
+++ include/Grafo.h
@@ -2,7 +2,9 @@
 #define GRAFO_H
 
 #include <string>
-
+#include <iostream>
+#include <fstream>
+#include <string>
 class Grafo {
 
     struct Aresta {
@@ -22,7 +24,10 @@ protected:
     bool direcionado;         // Define se o grafo é direcionado
     bool verticesPonderados;  // Define se os vértices possuem pesos
     bool arestasPonderadas;   // Define se as arestas possuem pesos
-
+    void dfs(int u, bool* visitado) ;
+    void getVizinhos(int u, int*& numVizinhos, int& tamanho) const;
+    virtual void inicializa_estrutura() = 0;
+    virtual void adiciona_aresta(int origem, int destino, int peso) = 0;
 public:
     // Construtor
     Grafo(int n, bool dir = false, bool vp = false, bool ap = false);
@@ -31,11 +36,11 @@ public:
     virtual ~Grafo();
 
     // Funções virtuais puras (abstrações)
-    virtual int n_conexo() = 0;
-    virtual int get_grau() = 0;
+    int n_conexo() ;
+   // virtual int get_grau() const;
     bool eh_completo() const;
-    virtual void carrega_grafo(const std::string& arquivo) = 0;
-    virtual void get_vizinhos(int v, int*& vizinhos, int& tamanho) = 0;
+    void carrega_grafo(const std::string& arquivo) ;
+    virtual void get_vizinhos(int v, int*& vizinhos, int& tamanho) const = 0;
     virtual void get_arestas(int*& arestas, int& tamanho) = 0;
 
     // Funções com implementação padrão
@@ -43,7 +48,8 @@ public:
     virtual bool eh_direcionado(); // Verifica se o grafo é direcionado
     virtual bool vertice_ponderado(); // Verifica se os vértices são ponderados
     virtual bool aresta_ponderada();  // Verifica se as arestas são ponderadas
-
+    //virtual int get_grau_vertice(int v) const; // Retorna o grau do vértice v
+    virtual int get_grau() ; // Retorna o grau do grafo
     virtual bool existeAresta(int u, int v) const = 0; // Verifica se existe uma aresta entre u e v
 
 };
--- include/GrafoLista.h
+++ include/GrafoLista.h
@@ -22,18 +22,21 @@ private:
     };
 
     Vertice* vertices;
-
+protected:
+    void inicializa_estrutura() override;
+    void adiciona_aresta(int origem, int destino, int peso) override;
 public:
     GrafoLista(int n, bool dir = false, bool vp = false, bool ap = false);
     ~GrafoLista();
 
-    void carrega_grafo(const std::string& arquivo) override;
-    void get_vizinhos(int v, int*& vizinhos, int& tamanho) override;
+    //void carrega_grafo(const std::string& arquivo) override;
+    void get_vizinhos(int v, int*& vizinhos, int& tamanho) const override;
     void get_arestas(int*& arestas, int& tamanho) override;
-    int n_conexo() override;
-    int get_grau() override;
+    //int n_conexo() override;
+    //int get_grau() override;
    // bool eh_completo() override;
     bool existeAresta(int u, int v) const override;
+    //int get_grau_vertice(int v) const override;
 };
 
 #endif // GRAFOLISTA_H
